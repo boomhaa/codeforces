94ae2948257d23a94d323ccc37a129bd7b883546
Владислав Тараканов <traker566@gmail.com>
2022-10-28 19:04:34 +0300

    new commit

--- Замена_чисел.py
+++ Замена_чисел.py
@@ -1,1 +1,14 @@
-print('hello_world')
+for i in range(int(input())):
+    my_hash = {}
+    n=int(input())
+    digits=list(map(int,input().split()))
+    letters=input()
+    ans='YES'
+    for j in range(n):
+        if digits[j] in my_hash:
+            if my_hash[digits[j]] !=letters[j]:
+                ans='NO'
+                break
+        else:
+            my_hash[digits[j]]=letters[j]
+    print(ans)
